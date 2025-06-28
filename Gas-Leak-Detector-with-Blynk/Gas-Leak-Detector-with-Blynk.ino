19b2e05794728a931f59468689136f398e52528f
Indusara Deviruwan <deviruvanindusara@gmail.com>
2025-06-28 12:21:01 +0530

    Code lines for Blynk app Intergration

--- Gas-Leak-Detector-with-Blynk/Gas-Leak-Detector-with-Blynk.ino
+++ Gas-Leak-Detector-with-Blynk/Gas-Leak-Detector-with-Blynk.ino
@@ -1,3 +1,8 @@
+#define BLYNK_TEMPLATE_ID "TMPL6rLAtS9tX"
+#define BLYNK_TEMPLATE_NAME "Gas Leak Detector"
+#define BLYNK_AUTH_TOKEN "RqbikRg-eqq5ooUAU-LHrf8-L9rR9yQy"
+
+
 void setup() {
   // put your setup code here, to run once:
 
